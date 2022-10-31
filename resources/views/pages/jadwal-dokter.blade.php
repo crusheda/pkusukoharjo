52b5e4ce2e4f998aba91584752d391fe251a08fd
Yussuf Faisal <thecrusheda@gmail.com>
2022-11-01 03:06:37 +0700

    API For Jadwal Dokter

--- resources/views/pages/jadwal-dokter.blade.php
+++ resources/views/pages/jadwal-dokter.blade.php
@@ -45,7 +45,7 @@
                     <tbody id="tampil-tbody"><tr><center><td colspan="7"><i class="fa fa-spinner fa-spin fa-fw"></i> Memproses data...</td></center></tr></tbody>
                 </table>
             </div>
-            <h6 class="mt-3">Jadwal sewaktu-waktu bisa berubah</h6>
+            <h6 class="mt-3">* Mohon maaf, jadwal sewaktu-waktu bisa berubah</h6>
         </div>
     </section>
     <!-- End Doctor Calendar Area -->
@@ -54,7 +54,6 @@
         $(document).ready(function() {
             $.ajax({
                 url: "http://103.155.246.25:8000/api/jadwaldokter/",
-                // url: "http://192.168.1.3:8000/api/all/"+this.value,
                 type: 'GET',
                 dataType: 'json',
                 success: function(res) {
@@ -62,9 +61,29 @@
                     if(res.length == 0){
                         $("#tampil-tbody").append(`<tr><td colspan="7"><center>Gagal memuat data</center></td></tr>`);
                     } else {
-                        // Array.from(res, item => {
-                        console.log(res);
-                        // Object.keys(res).forEach(item => {
+                        res.forEach(item => {
+                            content = "<tr id='data"+ item.id +"'>" 
+                                    + "<td style='text-align:left'><h3>" + item.NAMA + "</h3><span>" + item.SUBINSTALASI + " ("+item.REF_POLIBPJS+")</span></td>";
+                                    if (item.SENIN != null) { content += "<td>" + item.SENIN + "</td>" ; } else { content += "<td>-</td>"; }
+                                    if (item.SELASA != null) { content += "<td>" + item.SELASA + "</td>" ; } else { content += "<td>-</td>"; }
+                                    if (item.RABU != null) { content += "<td>" + item.RABU + "</td>" ; } else { content += "<td>-</td>"; }
+                                    if (item.KAMIS != null) { content += "<td>" + item.KAMIS + "</td>" ; } else { content += "<td>-</td>"; }
+                                    if (item.JUMAT != null) { content += "<td>" + item.JUMAT + "</td>" ; } else { content += "<td>-</td>"; }
+                                    if (item.SABTU != null) { content += "<td>" + item.SABTU + "</td>" ; } else { content += "<td>-</td>"; }
+                            $('#tampil-tbody').append(content);
+                        });
+                    }
+                }
+            });
+            $.ajax({
+                url: "http://192.168.1.3:8000/api/jadwaldokter/",
+                type: 'GET',
+                dataType: 'json',
+                success: function(res) {
+                    $("#tampil-tbody").empty();
+                    if(res.length == 0){
+                        $("#tampil-tbody").append(`<tr><td colspan="7"><center>Gagal memuat data</center></td></tr>`);
+                    } else {
                         res.forEach(item => {
                             content = "<tr id='data"+ item.id +"'>" 
                                     + "<td style='text-align:left'><h3>" + item.NAMA + "</h3><span>" + item.SUBINSTALASI + " ("+item.REF_POLIBPJS+")</span></td>";
